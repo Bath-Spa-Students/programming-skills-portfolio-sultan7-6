5c26883ee3030c3978131ddc35d5d07daee9ee54
sultan7-6 <144207482+sultan7-6@users.noreply.github.com>
2023-11-14 10:09:31 +0400

    Chapter4 exercise

---
+++ Chapter 4- Control Flow/Exercises/exercise3.py
@@ -0,0 +1,8 @@
+alien_color = 'pink'
+
+if alien_color == 'red':
+    print("You just earned 5 points!")
+elif alien_color == 'blue':
+    print("You just earned 10 points!")
+else:
+    print("You just earned 15 points!")
